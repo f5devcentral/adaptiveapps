288e17d246e6ab229cdba9619ec0bb40ad3ef464
Michael Kennedy <m.kennedy@f5.com>
2023-01-18 09:45:17 +1100

    cleaned up idea's
    Former-commit-id: 8d8dc0ca7f287c94a726743b7993a0d445a579a0 [formerly d26db997029d90864b66859b3abd373e2a2e6513]
    Former-commit-id: 622da2ff070f33b412e23824a2db2f555211ed0f

--- resources/f5xc-vk8s-mk8s-nlb/src/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "f5xc_app_url" {
-  description = "FQDN VIP to access the web app"
-  value       = module.f5xc_shop_demo.app_url
-}
--- resources/f5xc-vk8s-mk8s-nlb/src/providers.tf
+++
@@ -1,5 +0,0 @@
-provider "volterra" {
-  api_p12_file = "/path/to/api_credential.p12"
-  url          = "https://<tenant_name>.console.ves.volterra.io/api"
-  alias        = "tenancy"
-}
